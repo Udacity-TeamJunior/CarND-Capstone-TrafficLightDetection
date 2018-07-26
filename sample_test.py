a0b9ca8b5e71f6036fcc02584fb3fb56c0e7c282
Arthur Wu <wuqianliang@ismartearth.com>
2018-07-26 08:54:05 +0800

    Traffic lights detect

--- sample_test.py
+++ sample_test.py
@@ -1,128 +1,136 @@
 import numpy as np
 import os
-import six.moves.urllib as urllib
 import sys
-import tarfile
 import tensorflow as tf
-import zipfile
+import time
 
 from collections import defaultdict
 from io import StringIO
 from matplotlib import pyplot as plt
 from PIL import Image
 
-# This is needed since the notebook is stored in the object_detection folder.
-sys.path.append("..")
-from object_detection.utils import ops as utils_ops
-
-if tf.__version__ < '1.3.0':
-  raise ImportError('Please upgrade your tensorflow installation to v1.4.* or later!')
-
 from object_detection.utils import label_map_util
-
 from object_detection.utils import visualization_utils as vis_util
 
-NUM_CLASSES = 4
+#model_type = 'frcnn'
+model_type = 'ssd'
 
-PATH_TO_CKPT="/home/arthur/CarND-Capstone_team/branches/Dev_TL/modeltrain/model_frozen_real/frozen_inference_graph.pb"
-PATH_TO_LABELS="data/label_map.pbtxt"
+
+sim_model_path = 'model_frozen_sim/'+model_type+'/frozen_inference_graph.pb'
+real_model_path = 'model_frozen_real/'+model_type+'frozen_inference_graph.pb'
+labels_path = 'data/label_map.pbtxt'
+image_size = (12, 8)
+
+
+label_map = label_map_util.load_labelmap(labels_path)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=4, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+print(category_index)
 
 def load_image_into_numpy_array(image):
   (im_width, im_height) = image.size
   return np.array(image.getdata()).reshape(
       (im_height, im_width, 3)).astype(np.uint8)
 
-# Load a (frozen) Tensorflow model into memory.
-detection_graph = tf.Graph()
-with detection_graph.as_default():
-  od_graph_def = tf.GraphDef()
-  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-    serialized_graph = fid.read()
-    od_graph_def.ParseFromString(serialized_graph)
-    tf.import_graph_def(od_graph_def, name='')
 
-#  Loading label map
-label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-category_index = label_map_util.create_category_index(categories)
+from glob import glob
 
 
-TEST_IMAGE_PATHS = ['/home/arthur/CarND-Capstone_team/branches/Dev_TL/modeltrain/sampleimages/left0020.jpg','/home/arthur/CarND-Capstone_team/branches/Dev_TL/modeltrain/sampleimages/left0029.jpg']
-# Size, in inches, of the output images.
-IMAGE_SIZE = (12, 8)
-
-def run_inference_for_single_image(image, graph):
-  with graph.as_default():
-    with tf.Session() as sess:
-      # Get handles to input and output tensors
-      ops = tf.get_default_graph().get_operations()
-      all_tensor_names = {output.name for op in ops for output in op.outputs}
-      tensor_dict = {}
-      for key in [
-          'num_detections', 'detection_boxes', 'detection_scores',
-          'detection_classes', 'detection_masks'
-      ]:
-        tensor_name = key + ':0'
-        if tensor_name in all_tensor_names:
-          tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
-              tensor_name)
-      if 'detection_masks' in tensor_dict:
-        # The following processing is only for single image
-        detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
-        detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
-        # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
-        real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
-        detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
-        detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
-        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
-            detection_masks, detection_boxes, image.shape[0], image.shape[1])
-        detection_masks_reframed = tf.cast(tf.greater(detection_masks_reframed, 0.5), tf.uint8)
-        # Follow the convention by adding back the batch dimension
-        tensor_dict['detection_masks'] = tf.expand_dims(detection_masks_reframed, 0)
-
-      image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
-
-      # Run inference
-      output_dict = sess.run(tensor_dict,
-                             feed_dict={image_tensor: np.expand_dims(image, 0)})
-
-      # all outputs are float32 numpy arrays, so convert types as appropriate
-      output_dict['num_detections'] = int(output_dict['num_detections'][0])
-      output_dict['detection_classes'] = output_dict[
-          'detection_classes'][0].astype(np.uint8)
-      output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
-      output_dict['detection_scores'] = output_dict['detection_scores'][0]
-      if 'detection_masks' in output_dict:
-        output_dict['detection_masks'] = output_dict['detection_masks'][0]
-  return output_dict
-
-for image_path in TEST_IMAGE_PATHS:
-  image = Image.open(image_path)
-  print(image)
-  # the array based representation of the image will be used later in order to prepare the
-  # result image with boxes and labels on it.
-  image_np = load_image_into_numpy_array(image)
-  #image_np = image
-  # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-  image_np_expanded = np.expand_dims(image_np, axis=0)
-  #image_np_expanded = np.expand_dims(np.asarray(image_np, dtype=np.uint8), 0) 
-  # Actual detection.
-  output_dict = run_inference_for_single_image(image_np, detection_graph)
-  print(output_dict)
-  # Visualization of the results of a detection.
-  vis_util.visualize_boxes_and_labels_on_image_array(
-      image_np,
-      output_dict['detection_boxes'],
-      output_dict['detection_classes'],
-      output_dict['detection_scores'],
-      category_index,
-      instance_masks=output_dict.get('detection_masks'),
-      use_normalized_coordinates=True,
-      line_thickness=8)
-  plt.figure(figsize=IMAGE_SIZE)
-  plt.imshow(image_np)
+def import_graph(model_path):
+    detection_graph = tf.Graph()
+    
+    with detection_graph.as_default():
+      od_graph_def = tf.GraphDef()
+    
+      with tf.gfile.GFile(model_path, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+        
+    return detection_graph
+        
+    
+def load_test_images(directory):
+    print(os.path.join(directory, '*.jpg'))
+    image_paths = glob(os.path.join(directory, '*.jpg'))
+    print("Length of test images:", len(image_paths))
+    return image_paths
 
 
 
+def predict(detection_graph, image_paths):
+    with detection_graph.as_default():
+        with tf.Session(graph=detection_graph) as sess:
+            # Definite input and output Tensors for detection_graph
+            image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+            # Each box represents a part of the image where a particular object was detected.
+            detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+            # Each score represent how level of confidence for each of the objects.
+            # Score is shown on the result image, together with the class label.
+            detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+            detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+            num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+            for image_path in image_paths:
+                image = Image.open(image_path)
+                # the array based representation of the image will be used later in order to prepare the
+                # result image with boxes and labels on it.
+                image_np = load_image_into_numpy_array(image)
+                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+                image_np_expanded = np.expand_dims(image_np, axis=0)
+
+                time0 = time.time()
+
+                # Actual detection.
+                (boxes, scores, classes, num) = sess.run(
+                  [detection_boxes, detection_scores, detection_classes, num_detections],
+                  feed_dict={image_tensor: image_np_expanded})
+
+                time1 = time.time()
+
+                boxes = np.squeeze(boxes)
+                scores = np.squeeze(scores)
+                classes = np.squeeze(classes).astype(np.int32)
+
+                # Visualization of the results of a detection.
+                vis_util.visualize_boxes_and_labels_on_image_array(
+                    image_np, boxes, classes, scores,
+                    category_index,
+                    use_normalized_coordinates=True,
+                    line_thickness=6)
+
+                plt.figure(figsize=image_size)
+                plt.imshow(image_np)
+                plt.show()
+
+                min_score_thresh = .50
+                for i in range(boxes.shape[0]):
+                    if scores is None or scores[i] > min_score_thresh:
+
+                        class_name = category_index[classes[i]]['name']
+                        print('{}'.format(class_name), scores[i])
+
+                        fx =  0.97428
+                        fy =  1.73205
+                        perceived_width_x = (boxes[i][3] - boxes[i][1]) * 800
+                        perceived_width_y = (boxes[i][2] - boxes[i][0]) * 600
+
+                        # ymin, xmin, ymax, xmax = box
+                        # depth_prime = (width_real * focal) / perceived_width
+                        perceived_depth_x = ((.1 * fx) / perceived_width_x)
+                        perceived_depth_y = ((.3 * fy) / perceived_width_y )
+
+                        estimated_distance = round((perceived_depth_x + perceived_depth_y) / 2)
+                        print("Distance (metres)", estimated_distance)
+                        print("Time in milliseconds", (time1 - time0) * 1000, "\n")
 
 
+detection_graph = import_graph(sim_model_path)
+image_paths = load_test_images("/home/arthur/CarND-Capstone_team/branches/Dev_TL/modeltrain/sampleimages")
+predict(detection_graph, image_paths)
+'''
+detection_graph = import_graph(sim_model_path)
+image_paths = load_test_images("test_images_real")
+predict(detection_graph, image_paths)
+'''
